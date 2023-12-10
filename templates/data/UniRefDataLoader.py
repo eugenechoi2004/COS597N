ed05573a07759fa7bba25df3da7d62bd7a152f92
Eugene Choi <eugenechoi2004@gmail.com>
2023-12-10 16:38:19 -0500

    checkpoint

--- templates/data/UniRefDataLoader.py
+++ templates/data/UniRefDataLoader.py
@@ -1,33 +1,40 @@
 import torch
-from torchvision import datasets, transforms
-from .CustomDataLoader import CustomDataLoader
-import os
 from Bio import SeqIO
+import random
+import sys
+sys.path.append('../comp')
+from TokenizerBERT import TokenizerBERT
 
-class UniRefDataLoader(CustomDataLoader):
-
-    def __init__(self, path_to_data, batch_size):
+class UniRefDataLoader():
+    def __init__(self, path_to_data, batch_size, tokenizer:TokenizerBERT, max_length, mask_prob=0.15):
+        self.max_length = max_length
         self.path_to_data = path_to_data
-        self.max_length = 200
-        self.data = [record.seq for record in SeqIO.parse("raw_data/contigs-sub500_big.fasta", "fasta")]
+        self.mask_prob = mask_prob
         self.batch_size = batch_size
+        self.pad_token_id = 0  # Update this as per your padding token ID
+        self.mask_token_id = 103  # Update this if needed
+        self.data = [tokenizer.encode(record.seq) for record in SeqIO.parse(path_to_data, "fasta")]
 
-    def get_test_data_loader(self):
-        path = os.path.join(self.path_to_data,"raw_data/contigs-sub500_big.fasta)
-        trainset = self.data
-        trainloader = torch.utils.data.DataLoader(trainset, batch_size=self.batch_size, shuffle=True)
+    def mask_sequence(self, sequence):
+        masked_sequence = sequence.clone()
+        mask = (torch.rand(self.max_length) < self.mask_prob)
+        labels = torch.full([self.max_length], -100)  
+        for i in range(self.max_length):
+            if mask[i]:
+                masked_sequence[i] = self.mask_token_id
+                labels[i] = sequence[i]
+        return masked_sequence, labels
 
-    def get_train_data_loader(self):
-        path = os.path.join(self.path_to_data,"raw_data/contigs-sub500_big.fasta")
-        testset = self.data
-        testloader = torch.utils.data.DataLoader(testset, batch_size=self.batch_size, shuffle=True)
-        return testloader
+    def get_data_loader(self):
+        masked_data, labels = zip(*[self.mask_sequence(seq) for seq in self.data])
+        dataset = torch.utils.data.TensorDataset(torch.stack(masked_data), torch.stack(labels))
+        return torch.utils.data.DataLoader(dataset, batch_size=self.batch_size, shuffle=True)
 
+# Example usage
+tokenizer = TokenizerBERT("/scratch/network/OPEN-CATALYST/COS597N/templates/comp/vocab.json", 500)
+ul = UniRefDataLoader("raw_data/contigs-sub500_big.fasta", 64, tokenizer, 500)
+for masked_sequences, labels in ul.get_data_loader():
+    print(masked_sequences, labels)
+    break
 
-
-for record in SeqIO.parse("raw_data/contigs-sub500_big.fasta", "fasta"):
-    print("ID: %s" % record.id)
-    print("Sequence: %s" % record.seq)
-    record
-    
 
