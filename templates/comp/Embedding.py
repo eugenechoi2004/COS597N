8dcee4388e48b76c43357c77ecf49b05459e6b46
eugenechoi2004 <35019459+eugenechoi2004@users.noreply.github.com>
2023-12-10 21:55:58 -0500

    moved to gpu

--- templates/comp/Embedding.py
+++ templates/comp/Embedding.py
@@ -7,17 +7,23 @@ import numpy as np
 
 class Embedding(nn.Module):
     def __init__(self, vocab_size, embedding_dim, max_sequence_length, dropout):
-        super(Embedding, self).__init__()  # Call to base class __init__
+        super(Embedding, self).__init__()
         self.embeddings = nn.Embedding(vocab_size, embedding_dim)
-        self.positions_encodings = torch.zeros(max_sequence_length, embedding_dim) 
-        self.dropout = nn.Dropout(p=dropout)  
+        self.positions_encodings = torch.zeros(max_sequence_length, embedding_dim)
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        # Populate position encodings
         for row in range(len(self.positions_encodings)):
-            for col in range(len(self.positions_encodings[row])//2):
-                denominator = np.power(10_000, 2*col/embedding_dim)
-                self.positions_encodings[row,2*col] = np.sin(row/denominator)
-                self.positions_encodings[row,2*col + 1] = np.cos(row/denominator)
-        
+            for col in range(0, embedding_dim, 2):
+                denominator = math.pow(10000, (2 * col) / embedding_dim)
+                self.positions_encodings[row, col] = math.sin(row / denominator)
+                self.positions_encodings[row, col + 1] = math.cos(row / denominator)
+
+        self.positions_encodings = nn.Parameter(self.positions_encodings, requires_grad=False)  # Mark as non-trainable
+        self.positions_encodings.to(self.device)  # Move positions_encodings to specified device
+        self.dropout = nn.Dropout(p=dropout)
+
     def forward(self, x):
-        output = self.embeddings(x) + self.positions_encodings
+        x = self.embeddings(x)  # x will automatically be on the same device as embeddings
+        output = x + self.positions_encodings[:x.size(1), :]  # Adjust positions_encodings based on x's length
         return self.dropout(output)
 
--- templates/models/BERTModel.py
+++ templates/models/BERTModel.py
@@ -10,6 +10,7 @@ from comp.Embedding import Embedding
 class BERTModel(nn.Module):
     def __init__(self, max_sequence_length, embedding_dim, hidden_dim, dropout_prob, num_heads, num_layers, vocab_length):
         super(BERTModel, self).__init__()
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.embeddings = Embedding(vocab_size = vocab_length, embedding_dim = embedding_dim, max_sequence_length = max_sequence_length, dropout = dropout_prob)
         self.layers = nn.ModuleList([Encoder(max_sequence_length, embedding_dim, hidden_dim, dropout_prob, num_heads, vocab_length) for _ in range(num_layers)])
         self.final_linear = nn.Linear(embedding_dim, vocab_length)  # Linear layer to project back to vocab space
@@ -21,20 +22,3 @@ class BERTModel(nn.Module):
             x = layer(x)
         x = self.final_linear(x)  
         return x
-
-# Hyperparams
-embedding_dim = 768 # from paper
-num_encoder_layers = 6 # from paper
-max_sequence_length = 500
-hidden_dim = 3072 # from paper
-dropout_prob = 0.2
-num_heads = 12 # from paper
-mlm_prob = 0.15
-vocab_length = 30
-batch_size = 10
-# 15% of rht mlm batch_size 10 ~ 11 epochs
-
-
-
-model = BERTModel(max_sequence_length, embedding_dim, hidden_dim, dropout_prob, num_heads, num_encoder_layers, vocab_length)
-
