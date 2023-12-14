06ad507c3e8a8051427d7e8dd26804f34ce7e988
eugenechoi2004 <35019459+eugenechoi2004@users.noreply.github.com>
2023-12-14 15:20:00 -0500

    added in 100k

--- templates/scratch/run.py
+++ templates/scratch/run.py
@@ -1,4 +1,14 @@
-from Bio.Align.Applications import MuscleCommandline
-from Bio import AlignIO
 from Bio import SeqIO
-from io import StringIO
+
+def subset_fasta(input_file, output_file, max_sequences):
+    with open(input_file, "r") as infile, open(output_file, "w") as outfile:
+        for i, record in enumerate(SeqIO.parse(infile, "fasta")):
+            if i >= max_sequences:
+                break
+            SeqIO.write(record, outfile, "fasta")
+
+input_fasta = "../data/raw_data/2mil.fasta"  # Path to your original FASTA file
+output_fasta = "../data/raw_data/100k.fasta"  # Path to the output FASTA file
+max_sequences = 100_000  # Number of sequences to keep
+
+subset_fasta(input_fasta, output_fasta, max_sequences)
